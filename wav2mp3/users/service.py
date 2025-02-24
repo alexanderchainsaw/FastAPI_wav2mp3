7c912665dccaa5ddc75a77d9c18aa70967c8cf3d
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2025-02-24 09:30:44 +0300

    update validate_credentials - now gonna use it with depends for auto-check of user_id and token correlation, returing User object if successful

--- wav2mp3/users/service.py
+++ wav2mp3/users/service.py
@@ -1,15 +1,21 @@
+from typing import Annotated
 from uuid import uuid4
 
+from fastapi import Depends
 from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ..database.core import get_db
 from ..users.models import User, UserCreate
 
 
-async def validate_credentials(session: AsyncSession, user_id: str, token: str) -> None:
+async def validate_credentials(
+    session: Annotated[AsyncSession, Depends(get_db)], user_id: str, token: str
+) -> User | None:
     user = await session.get(User, user_id)
     if not user or user.token != token:
         raise HTTPException(status_code=401, detail="Invalid credentials")
+    return user
 
 
 async def create_user(session: AsyncSession, user_create: UserCreate) -> User:
